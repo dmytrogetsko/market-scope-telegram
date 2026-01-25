cd3a3579430059acbcc71cd5c732fd27b1bc17a0
dmytrohetsko <dmytro.getsko@beleven.dev>
2026-01-25 15:20:43 +0200

    fix(env): Add fixes to pre-push

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
@@ -29,7 +30,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function ($user = null) {
+        Gate::define('viewHorizon', function (User $user) {
             return in_array(optional($user)->email, [
                 //
             ]);
